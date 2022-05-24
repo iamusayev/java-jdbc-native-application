20385018ec1a434fd974dbdff86a359189041ca7
iamusayev <iamusayev1337@gmail.com>
2022-05-24 18:59:30 +0400

    Added Dao exception

---
+++ src/exception/DaoException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class DaoException extends RuntimeException{
+
+    private final String code;
+
+    public DaoException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+}
