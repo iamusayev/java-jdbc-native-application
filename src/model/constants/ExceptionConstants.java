b65d49793bd9cf45825aaa887c217fc91fd153f1
iamusayev <iamusayev1337@gmail.com>
2022-05-24 18:59:58 +0400

    Added Exception constants

---
+++ src/model/constants/ExceptionConstants.java
@@ -0,0 +1,8 @@
+package model.constants;
+
+public class ExceptionConstants {
+
+    public static final String DAO_EXCEPTION_CODE = "DAO_EXCEPTION";
+    public static final String DAO_EXCEPTION_MESSAGE = "HAVING PROBLEMS WITH THE DATABASE";
+
+}
