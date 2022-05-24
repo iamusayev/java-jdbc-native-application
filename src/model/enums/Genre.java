d6c37674564197e98b2ab430d51b2f71ec1c2243
iamusayev <iamusayev1337@gmail.com>
2022-05-24 19:00:23 +0400

    Added Genre

---
+++ src/model/enums/Genre.java
@@ -0,0 +1,15 @@
+package model.enums;
+
+public enum Genre {
+
+    TRAVEL,
+    STORY,
+    LEGEND,
+    HISTORY,
+    SELF_DEVELOPMENT,
+    COMPUTER_SCIENCE,
+    PROGRAMMING,
+    NOVELS,
+    AMATEUR_LITERATURE
+
+}
